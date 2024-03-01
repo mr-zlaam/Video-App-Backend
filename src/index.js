34f159f0ced00946aca3983e0a51445076adbf33
Zlaam <mrzalaam@gmail.com>
2024-03-01 18:44:44 +0500

    update app.js and src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `😋 Server is running at PORT::http://localhost:${process.env.PORT}/`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(" 🤢 Error connecting to database ::", error);
+    throw error;
+  });
