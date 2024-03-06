2aa93ca015b4eb420711757b29d090d04c3f10f9
Zlaam <mrzalaam@gmail.com>
2024-03-06 18:38:12 +0500

    last stable commit

--- src/app.js
+++ src/app.js
@@ -7,4 +7,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+import userRouter from "./routes/user.routes.js";
+//routes declaration
+app.use("/user", userRouter);
 export { app };
